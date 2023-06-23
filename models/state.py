886319cfc70628dd2d2851f7129b22ded83a0eba
David Gregs <108700012+davidgregs87@users.noreply.github.com>
2023-06-23 14:01:16 +0100

    Update state.py

--- models/state.py
+++ models/state.py
@@ -5,14 +5,4 @@ from models.engine.file_storage import storage
 
 class State(BaseModel, Base):
     """ State class """
-    __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-    cities = relationship('City', cascade='all, delete')
-
-    def cities(self):
-        c_list = []
-        c_dict = storage.all(City)
-        for k, v in c.dict.items():
-            if v.state_id == self.id:
-				c_list.append(v)
-		return c_list
+    name = ""
