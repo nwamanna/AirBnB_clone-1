97836180476da241a8330e5c8e1dc0f9cea10ead
David Gregs <108700012+davidgregs87@users.noreply.github.com>
2023-06-24 11:58:03 +0100

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,13 +1,29 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage"""
-import os
-storage_type = os.getenviron('HBNB_TYPE_STORAGE')
+'''
+    Package initializer
+'''
+from os import getenv
+
+from models.state import State
+from models.city import City
+from models.user import User
+from models.review import Review
+from models.base_model import BaseModel
+from models.amenity import Amenity
+from models.place import Place
+
+
+classes = {"User": User, "BaseModel": BaseModel,
+           "Place": Place, "State": State,
+           "City": City, "Amenity": Amenity,
+           "Review": Review}
+
+storage_type = getenv("HBNB_TYPE_STORAGE")
 
 if storage_type == 'db':
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
-    storage.reload()
 else:
     from models.engine.file_storage import FileStorage
     storage = FileStorage()
-    storage.reload()
+storage.reload()
