9276e3c84d885441138a1850471c9eafa5ac1745
Nwamanna <nwamannazika@gmail.com>
2023-07-22 10:50:01 +0100

    Flask web framework

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """ /: display “Hello HBNB! """
-from web_flask import app
+from flask import Flask
+
+app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_world():
